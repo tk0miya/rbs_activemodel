cf47a4d477d9b0d299e71a85d9a9075ee0c2591d
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-08-20 19:43:52 +0900

    Initial commit (Just after "bundle gem rbs_activemodel")

---
+++ lib/rbs_activemodel.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbs_activemodel/version"
+
+module RbsActivemodel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbs_activemodel/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RbsActivemodel
+  VERSION = "0.1.0"
+end
